3c5221deed2b6929372d51f65f40aaaced92515c
karan <karan.1123@zenmonk.tech>
2024-05-17 13:13:22 +0530

    User Apis done

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { signupUser } from "../controllers/userController.js";
+import {
+  signupUser,
+  loginUser,
+  logoutUser,
+  followUnFollowUser,
+  updateUser,
+  getUserProfile,
+} from "../controllers/userController.js";
+import protectRoute from "../middlewares/protectRoute.js";
 const router = express.Router();
 
+router.get("/profile/:query", getUserProfile);
 router.post("/signup", signupUser);
-// router.post("/login", loginUser);
-// router.post("/logout", logoutUser);
-
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/follow/:id", protectRoute, followUnFollowUser); // Toggle state(follow/unfollow)
+router.put("/update/:id", protectRoute, updateUser);
 
 export default router;
